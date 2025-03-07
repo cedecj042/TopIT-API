a5d8f905a7ef03447cb6ec02d4c5483448642962
cedecj042 <cedricoporto1st@gmail.com>
2025-03-07 08:42:18 +0800

    feat:Added dockerfile and docker-compose

--- Setup.py
+++ Setup.py
@@ -4,6 +4,8 @@ import layoutparser as lp
 from langchain_openai import ChatOpenAI
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
+from chromadb import PersistentClient, HttpClient
+import chromadb
 # from langchain_community.chat_models import ChatOpenAI
 from langchain.schema import Document
 from sentence_transformers import SentenceTransformer
@@ -44,7 +46,7 @@ API_KEY_LLAMAPARSE = os.getenv("LLAMA_CLOUD_API_KEY")
 repo_id = "lepuer/layout_parser"
 config_path = hf_hub_download(repo_id=repo_id, filename="config.yaml")
 model_path = hf_hub_download(repo_id=repo_id, filename="model_final.pth")
-
+chroma_client = HttpClient(host="chromadb", port=8000)
 
 # Initialize Detectron2LayoutModel using downloaded files
 DETECTRON_MODEL = lp.Detectron2LayoutModel(
@@ -67,10 +69,6 @@ FASTER_RCNN_CONFIG_PATH = hf_hub_download(repo_id=FASTER_RCNN_REPO_ID, filename=
 CHROMA_PERSIST_DIR = os.getenv('CHROMA_PERSIST_DIR')
 NLTK_DATA_DIR = os.getenv('NLTK_DATA_DIR')
 
-LABEL_ENCODER_PATH = os.getenv('LABEL_ENCODER_PATH')
-FEATURE_STATS_PATH = os.getenv('FEATURE_STATS_PATH')
-DISTILBERT_MODEL_PATH = os.getenv('DISTILBERT_MODEL_PATH')
-DISTILBERT_TOKENIZER_PATH = os.getenv('DISTILBERT_TOKENIZER_PATH')
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # HEADERS = {"Authorization": f"Bearer {API_KEY}"}
 
@@ -79,14 +77,6 @@ PARSER = LlamaParse(
     api_key=API_KEY_LLAMAPARSE,
     result_type="text"
 )
-
-# DETECTRON_MODEL = lp.Detectron2LayoutModel(
-#     config_path=FASTER_RCNN_CONFIG_PATH,
-#     model_path=FASTER_RCNN_MODEL_PATH,
-#     extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.7],
-#     label_map={0: "Caption", 1: "Code", 2: "Figures", 3: "Header", 4: "Lesson", 5: "Module", 6: "Section", 7: "Subsection", 8: "Tables", 9: "Text"}
-# )
-
 EMBEDDING_MODEL = SentenceTransformer('all-MiniLM-L6-v2') 
 
 MODEL_NAME = 'sentence-transformers/all-MiniLM-L6-v2'
@@ -99,24 +89,22 @@ SBERT = HuggingFaceEmbeddings(
     encode_kwargs=ENCODE_KWARGS
 )
 
-
 # question difficulty estimation model 
-MAX_LEN = 128
-LABEL_ENCODER = joblib.load(LABEL_ENCODER_PATH)
-LABEL_CLASSES = len(LABEL_ENCODER.classes_) 
-FEATURE_STATS = joblib.load(FEATURE_STATS_PATH)
-
 SCALER = joblib.load("RandomForest/updated_scaler.pkl")
 TFIDF_VECTORIZER = joblib.load("RandomForest/updated_tfidf_vectorizer.pkl")
 RANDOM_FOREST_MODEL = joblib.load("RandomForest/updated_trained_model.pkl")
+client = chromadb.PersistentClient(path="/chroma_db")
+print(client.list_collections())
 
 CONTENT_DOCUMENT = Chroma(
+    # client=chroma_client,
     collection_name="TopIT1",
     embedding_function=SBERT,
     persist_directory=CHROMA_PERSIST_DIR,
 )
 
 QUESTION_DOCUMENT = Chroma(
+    # client=chroma_client,
     collection_name="Questions1",
     embedding_function=SBERT,
     persist_directory=CHROMA_PERSIST_DIR
--- dockerfile
+++ dockerfile
@@ -1,6 +1,4 @@
-# Use an official PyTorch image with CUDA (for GPU) or Python base (for CPU)
-# FROM pytorch/pytorch:2.1.0-cuda11.8-cudnn8-runtime  # For GPU
-FROM python:3.10
+FROM python:3.10.12
 
 # Install system dependencies
 RUN apt-get update && apt-get install -y \
@@ -10,13 +8,17 @@ RUN apt-get update && apt-get install -y \
     git \
     curl \
     libgl1-mesa-glx \
-    && apt-get clean
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
 
 # Set the working directory
 WORKDIR /app
 
 # Upgrade pip
 RUN python3 -m pip install --upgrade pip
+
+# Copy requirements and install
+COPY requirements.txt .
+RUN pip install -r requirements.txt
 
 # Install PyTorch and torchvision
 RUN pip install torch torchvision
@@ -26,12 +28,3 @@ RUN pip install 'git+https://github.com/facebookresearch/detectron2.git'
 
 # Copy project files
 COPY . .
-
-# Install remaining Python dependencies
-RUN pip install --no-cache-dir -r requirements.txt
-
-# Expose FastAPI port
-EXPOSE 8001
-
-# Run FastAPI with Uvicorn
-CMD ["uvicorn", "Main:app", "--host", "0.0.0.0", "--port", "8001"]
